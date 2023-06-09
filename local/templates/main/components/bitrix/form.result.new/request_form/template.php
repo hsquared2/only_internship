5f9581f0fe2beb648a68e03773ce9ba758f139bf
hsquared2 <48555785+hsquared2@users.noreply.github.com>
2023-06-09 13:06:24 +0400

    Update template.php

--- local/templates/main/components/bitrix/form.result.new/request_form/template.php
+++ local/templates/main/components/bitrix/form.result.new/request_form/template.php
@@ -26,51 +26,38 @@ if ($arResult["isFormErrors"] == "Y"):?><?=$arResult["FORM_ERRORS_TEXT"];?><?end
 		else
 		{
 	?>
-    <?if($FIELD_SID !== "MESSAGE"):?>
-      <div class="contact-form__form-inputs">
-          <div class="input contact-form__input">
-            <label class="input__label" for="medicine_name">
-              <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if ($arQuestion["REQUIRED"] == "Y"):?><?=$arResult["REQUIRED_SIGN"];?><?endif;?></div>
-                <?=$arQuestion["HTML_CODE"]?>
-              <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
-            </label>
-          </div>
-      </div>
-    <?else:?>
-      <div class="contact-form__form-message">
-            <div class="input">
-              <label class="input__label" for="medicine_message">
-              <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if ($arQuestion["REQUIRED"] == "Y"):?><?=$arResult["REQUIRED_SIGN"];?><?endif;?></div>
-                <?=$arQuestion["HTML_CODE"]?>
-								<div class="input__notification"></div>
-              </label>
-            </div>
-      </div>
-    <?endif;?>
-    <?php
-      continue;
-    ?>
-		<tr>
-			<td>
-				<?if (is_array($arResult["FORM_ERRORS"]) && array_key_exists($FIELD_SID, $arResult['FORM_ERRORS'])):?>
-				<span class="error-fld" title="<?=htmlspecialcharsbx($arResult["FORM_ERRORS"][$FIELD_SID])?>"></span>
-				<?endif;?>
-				<?=$arQuestion["CAPTION"]?><?if ($arQuestion["REQUIRED"] == "Y"):?><?=$arResult["REQUIRED_SIGN"];?><?endif;?>
-				<?=$arQuestion["IS_INPUT_CAPTION_IMAGE"] == "Y" ? "<br />".$arQuestion["IMAGE"]["HTML_CODE"] : ""?>
-			</td>
-			<td><?=$arQuestion["HTML_CODE"]?></td>
-		</tr>
+	    <?if($FIELD_SID !== "MESSAGE"):?>
+	      <div class="contact-form__form-inputs">
+		  <div class="input contact-form__input">
+		    <label class="input__label" for="medicine_name">
+		      <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if ($arQuestion["REQUIRED"] == "Y"):?><?=$arResult["REQUIRED_SIGN"];?><?endif;?></div>
+			<?=$arQuestion["HTML_CODE"]?>
+		      <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+		    </label>
+		  </div>
+	      </div>
+	    <?else:?>
+	      <div class="contact-form__form-message">
+		    <div class="input">
+		      <label class="input__label" for="medicine_message">
+		      <div class="input__label-text"><?=$arQuestion["CAPTION"]?><?if ($arQuestion["REQUIRED"] == "Y"):?><?=$arResult["REQUIRED_SIGN"];?><?endif;?></div>
+			<?=$arQuestion["HTML_CODE"]?>
+									<div class="input__notification"></div>
+		      </label>
+		    </div>
+	      </div>
+	    <?endif;?>
 	<?
 		}
 	} //endwhile
 	?>
-		<div class="contact-form__bottom">
-				<div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
-						ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
-						данных&raquo;.
-				</div>
-				<input class="form-button contact-form__bottom-button" <?=(intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : "");?> type="submit" name="web_form_submit" value="<?=htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]);?>" />
-		</div>
+	<div class="contact-form__bottom">
+			<div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
+					ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
+					данных&raquo;.
+			</div>
+			<input class="form-button contact-form__bottom-button" <?=(intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : "");?> type="submit" name="web_form_submit" value="<?=htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]);?>" />
+	</div>
 <?=$arResult["REQUIRED_SIGN"];?> - <?=GetMessage("FORM_REQUIRED_FIELDS")?>
 </p>
 <?=$arResult["FORM_FOOTER"]?>

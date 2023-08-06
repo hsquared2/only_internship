b2bd1486c37394db38cd082c8b1233eb6a88ebdc
hsquared2 <48555785+hsquared2@users.noreply.github.com>
2023-08-07 02:52:10 +0400

    Add files via upload

---
+++ my.module/lang/en/install/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS['IEX_CPROP_MODULE_NAME'] = 'Complex property of infoblock';
+$MESS['IEX_CPROP_MODULE_DESC'] = 'Complex property (many fields in one property)';
+$MESS['IEX_CPROP_PARTNER_NAME'] = 'my.module';
+$MESS['IEX_CPROP_INSTALL_ERROR_VERSION'] = 'Please update the version of bitrix';
---
+++ my.module/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS['IEX_CPROP_MODULE_NAME'] = 'Комплексное свойство инфоблока';
+$MESS['IEX_CPROP_MODULE_DESC'] = 'Комплексное свойство (много полей в одном свойстве)';
+$MESS['IEX_CPROP_PARTNER_NAME'] = 'my.module';
+$MESS['IEX_CPROP_INSTALL_ERROR_VERSION'] = 'Пожалуйста обновите версию битрикса';
